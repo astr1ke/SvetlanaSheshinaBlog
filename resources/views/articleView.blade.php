2eed33cce1fe7f57cf279a1d7aa251448d8d2297
astrike <astrike_pro@users.noreply.github.com>
2018-06-17 00:46:34 +0300

    e=fix5

--- resources/views/articleView.blade.php
+++ resources/views/articleView.blade.php
@@ -16,8 +16,6 @@
 @section('content')
     <script type="text/javascript" src="{{asset('modules/lightbox')}}/scripts/jquery-1.4.2.min.js"></script>
     <script type="text/javascript" src="{{asset('modules/lightbox')}}/scripts/jquery.lightbox.js"></script>
-    <script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
-    <script src="//yastatic.net/share2/share.js"></script>
 
 
 
@@ -130,7 +128,16 @@
                                             <p>{!! $articles->text !!}</p>
                                         </div>
                                         <div class="post-share" style="text-align: center">
-                                            <div class="ya-share2" async="async"  data-services="vkontakte,odnoklassniki,gplus,whatsapp,telegram"></div>
+                                            <script type="text/javascript">(function() {
+                                                    if (window.pluso)if (typeof window.pluso.start == "function") return;
+                                                    if (window.ifpluso==undefined) { window.ifpluso = 1;
+                                                        var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+                                                        s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+                                                        s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+                                                        var h=d[g]('body')[0];
+                                                        h.appendChild(s);
+                                                    }})();</script>
+                                            <div class="pluso" data-background="#ebebeb" data-options="medium,square,line,horizontal,counter,theme=04" data-services="vkontakte,odnoklassniki,google,twitter,facebook,moimir,print,email"></div>
                                         </div>
                                         <div class="tags-meta-and-others clearfix">
                                             <div class="post-tags">
